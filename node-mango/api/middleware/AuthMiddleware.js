06442e31a1924bb94f477fa8b8bca6b40740f387
oshadaera68 <mindartlk010@gmail.com>
2023-10-25 22:03:19 +0530

    updated

--- node-mango/api/middleware/AuthMiddleware.js
+++ node-mango/api/middleware/AuthMiddleware.js
@@ -1,7 +1,27 @@
-const jsonWebToken = require('jsonwebtoken');
+const jsonWebToken = require("jsonwebtoken");
 
+function verifyToekn(req, res, next) {
+  const authorizeHeader = req.headers.authorization;
+  if (!authorizeHeader) {
+    return res.status(401).json({ error: "no token provided." });
+  }
 
-function verifyToekn(req,resp,next){
-console.log(req.headers.authorization);
+  if (!authorizeHeader.startsWith("Bearer")) {
+    return res.status(401).json({ error: "invalid token format." });
+  }
+
+  const token = authorizeHeader.slice(7);
+
+  if(!token){
+    return res.status(401).json({error:'invalid token.'});
+  }
+
+  try{
+        const decodedData = jsonWebToken.verify(token, process.env.SECRET_KEY);
+        console.log(decodedData);
+        next();
+  }catch(error){
+    return res.status(401).json({error:'invalid token.'});
+  }
 }
-module.exports=verifyToekn;
+module.exports = verifyToekn;
