44d7cd0bbfb4a72f86509618eddec52608056ebb
oshadaera68 <mindartlk010@gmail.com>
2023-11-29 20:22:59 +0530

    added new project

--- CustomerApp/database.sql
+++ CustomerApp/database.sql
@@ -1,17 +1,47 @@
-DROP DATABASE IF EXISTS node_customer;
-CREATE DATABASE IF NOT EXISTS node_customer;
-USE node_customer;
-#=============================
-CREATE TABLE IF NOT EXISTS customer(
-    nic VARCHAR(20) PRIMARY KEY,
-    name VARCHAR(45) NOT NULL,
-    address VARCHAR(100) NOT NULL,
-    salary DOUBLE
+/* Dropping and Creating database */
+DROP DATABASE IF EXISTS studentCrudApp;
+CREATE DATABASE IF NOT EXISTS studentCrudApp;
+SHOW DATABASES;
+USE studentCrudApp;
+
+/* Creating Tables */
+DROP TABLE IF EXISTS student;
+CREATE TABLE IF NOT EXISTS student(
+    stuId VARCHAR(15) PRIMARY KEY,
+    name VARCHAR(45),
+    address TEXT,
+    email VARCHAR(100),
+    contact_no VARCHAR(11)
 );
 SHOW TABLES;
-DESC customer;
-INSERT INTO customer VALUES('95','Kamal','Colombo',25000),
-('985','Jagath','Kalutara',80000),
-('785','Wasantha','Panadura',45000);
+DESC student;
 
-SELECT * FROM customer;
+DROP TABLE IF EXISTS program;
+CREATE TABLE IF NOT EXISTS program(
+    proId VARCHAR(15) PRIMARY KEY,
+    proName VARCHAR(50),
+    duration TIME,
+    cost DOUBLE,
+    studentId VARCHAR(15),
+    CONSTRAINT FOREIGN KEY (studentId) REFERENCES student(stuId)
+);
+SHOW TABLES;
+DESC program;
+
+DROP TABLE IF EXISTS lecturer;
+CREATE TABLE IF NOT EXISTS lecturer(
+    lecturerId VARCHAR(15) PRIMARY KEY,
+    lecturerName VARCHAR(50),
+    programId VARCHAR(15),
+    CONSTRAINT FOREIGN KEY (programId) REFERENCES program(proId)
+);
+SHOW TABLES;
+DESC lecturer;
+
+DROP TABLE IF EXISTS registration;
+CREATE TABLE IF NOT EXISTS registration(
+    regId VARCHAR(15) PRIMARY KEY,
+    regDate DATE
+);
+SHOW TABLES;
+DESC registration;
