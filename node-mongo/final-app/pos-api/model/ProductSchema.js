6f949f7664f691ef59c68dc6005adbabea044190
oshadaera68 <mindartlk010@gmail.com>
2023-12-02 22:22:46 +0530

    updated content of schemas

---
+++ node-mongo/final-app/pos-api/model/ProductSchema.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+const ProductSchema = new mongoose.Schema({
+    name:{
+        type:String,
+        required:true
+    },
+    description:{
+        type:String,
+        required:true
+    },
+    unitPrice:{
+        type:Number,
+        required:true
+    },
+    qtyOnHand:{
+        type:Number,
+        required:true
+    }
+});
+module.exports = mongoose.model('product', ProductSchema);
